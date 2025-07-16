39496022c67edeccfd4f0ea1da59548c6f107915
Brayan Denil <bsacalxute@miumg.edu.gt>
2025-07-15 21:29:15 -0600

    primera version del sistema de mascotas

---
+++ src/main/java/com/mycompany/veterinaria2/Mascotas.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.veterinaria2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Mascotas {
+     
+    private String nombre;
+    private String especie;
+    private int edad;
+    private boolean vacunado;
+    
+    public Mascotas(String nombre,String especie,int edad,boolean vacunado){
+    this.nombre= nombre;
+    this.especie= especie;
+    this.edad= edad;
+    this.vacunado=vacunado;
+    
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getEspecie(){
+    return especie;
+    
+    
+    }
+    public void setEspecie(){
+    this.especie= especie;
+    
+    
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isVacunado() {
+        return vacunado;
+    }
+
+    public void setVacunado(boolean vacunado) {
+        this.vacunado = vacunado;
+    }
+    
+    
+    public void mostrarInformacion(){
+      System.out.println("Nombre: " + nombre + ", Especie: " + especie + ", Edad: " + edad);
+        System.out.println("Vacunado:"+vacunado);      
+      
+      
+      
+    };
+    
+    
+    
+    
+    }
+    
